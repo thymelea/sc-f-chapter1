dc23cd83b8772a6060bf0e75e0604b4b27c977bb
thymelea <996300709@qq.com>
2018-07-17 11:36:25 +0800

    init

--- service-ribbon/src/main/java/com/forezp/serviceribbon/serverImp/TestServerImp.java
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/serverImp/TestServerImp.java
@@ -2,6 +2,7 @@ package com.forezp.serviceribbon.serverImp;
 
 
 import com.forezp.serviceribbon.server.TestServer;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +12,13 @@ public class TestServerImp implements TestServer {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     @Override
     public String testFun(String name) {
 
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
